2a104ab931da25c6d8a6fa6f9136dffa1828a322
Manoo07 <manoharboinapalli1196@gmail.com>
2026-06-04 09:12:44 +0530

    deploy: update frontend

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,10 +3,7 @@ import SidebarSkeleton from './SidebarSkeleton'
 import { toast } from '../../store/useToastStore'
 import { createPortal } from 'react-dom'
 import {
-  Sun,
-  CalendarRange,
   Plus,
-  CheckCircle2,
   Trash2,
   Tag,
   ChevronRight,
